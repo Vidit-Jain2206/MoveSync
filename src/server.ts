0132639f8b988408800d64dd5ac5b5e0801cf120
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-02-07 23:40:54 +0530

    Integrated redis pub/sub

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import Redis from "ioredis";
 
+const pub = new Redis({
+  host: "localhost",
+  port: 6379,
+});
+const sub = new Redis({
+  host: "localhost",
+  port: 6379,
+});
+
 const app = createServer();
 const io = new Server(app, {
   cors: {
@@ -16,23 +25,39 @@ io.on("connection", (socket) => {
     socket.data.role = role;
     socket.data.roomId = roomId;
     console.log(`${role} joined room: ${roomId}`);
+    if (role === "user") {
+      sub.subscribe(`location:${roomId}`);
+    }
   });
 
   socket.on("update-location", (location: { lat: number; lng: number }) => {
-    console.log("update-location", location);
     const roomId = socket.data.roomId;
-    console.log(roomId);
-    // if (!roomId || socket.data.role !== "driver") return;
-    socket.to(roomId).emit("driver-location", location);
+    if (!roomId || socket.data.role !== "driver") return;
+
+    // Publish to Redis
+    pub.publish(
+      `location:${roomId}`,
+      JSON.stringify({
+        location,
+        timestamp: Date.now(),
+      })
+    );
   });
 
   socket.on("disconnect", () => {
     const roomId = socket.data.roomId;
     if (!roomId) return;
     socket.leave(roomId);
-
+    if (socket.data.role === "user") {
+      sub.unsubscribe(`location:${roomId}`);
+    }
     console.log(`${socket.data.role} left room: ${roomId}`);
   });
+});
+
+sub.on("message", (channel, message) => {
+  const roomId = channel.split(":")[1];
+  io.to(roomId).emit("driver-location", JSON.parse(message));
 });
 
 app.listen(3000, () => {
