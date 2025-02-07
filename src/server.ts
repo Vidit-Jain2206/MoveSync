ddde5f936d4b4da70448d76cc248a4a1d6c55b56
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-02-07 23:40:20 +0530

    integrated user and driver in socket

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,6 @@ import { createServer } from "http";
 import { Server } from "socket.io";
 import Redis from "ioredis";
 
-const pub = new Redis();
-const sub = new Redis();
-
 const app = createServer();
 const io = new Server(app, {
   cors: {
@@ -14,6 +11,28 @@ const io = new Server(app, {
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+  socket.on("join-room", (roomId: string, role: "driver" | "user") => {
+    socket.join(roomId);
+    socket.data.role = role;
+    socket.data.roomId = roomId;
+    console.log(`${role} joined room: ${roomId}`);
+  });
+
+  socket.on("update-location", (location: { lat: number; lng: number }) => {
+    console.log("update-location", location);
+    const roomId = socket.data.roomId;
+    console.log(roomId);
+    // if (!roomId || socket.data.role !== "driver") return;
+    socket.to(roomId).emit("driver-location", location);
+  });
+
+  socket.on("disconnect", () => {
+    const roomId = socket.data.roomId;
+    if (!roomId) return;
+    socket.leave(roomId);
+
+    console.log(`${socket.data.role} left room: ${roomId}`);
+  });
 });
 
 app.listen(3000, () => {
