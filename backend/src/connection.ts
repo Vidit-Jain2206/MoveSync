7c65379b1b32246cec80664a53321d212aa3c3ca
Vidit-Jain2206 <jainvidit203@gmail.com>
2025-02-19 00:50:53 +0530

    added customer frontend application basic setup

--- backend/src/connection.ts
+++ backend/src/connection.ts
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 export const connection = () => {
-  mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/");
+  mongoose.connect(
+    process.env.MONGO_URI || "mongodb://admin:secret@localhost:27017"
+  );
 
   mongoose.connection.on("error", (err) => {
     console.error("MongoDB connection error:", err);
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,92 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import io, { Socket } from "socket.io-client";
 
+const ENDPOINT = "http://localhost:3001";
+interface Location {
+  lat: number;
+  lng: number;
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const [userLocation, setUserLocation] = useState<Location>({
+    lat: 0,
+    lng: 0,
+  });
+  const [driverLocation, setDriverLocation] = useState<Location>({
+    lat: 0,
+    lng: 0,
+  });
+  const [orderId, setOrderId] = useState("");
+  const [socket, setSocket] = useState<Socket>();
 
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+      setUserLocation({
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      });
+    });
+  }, []);
+
+  useEffect(() => {
+    const socket = io(ENDPOINT);
+    setSocket(socket);
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleJoinRoom = async () => {
+    try {
+      if (socket) {
+        const orderId = Math.floor(Math.random() * 10000000).toString();
+        socket.emit("join-room");
+        setOrderId(orderId);
+      }
+    } catch (error) {
+      console.error("Failed to join room:", error);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="w-screen h-screen flex flex-col bg-[#8da4f1]">
+      {/* navbar */}
+      <div className="w-full p-4 bg-[#403d71] text-[#8da4f1] font-bold text-3xl">
+        <h1 className="text-center">Welcome to Movesync</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      {/* mainsection */}
+      <div className="w-[95%] md:w-[80%] lg:w-[60%] mx-auto mt-[2rem]">
+        {/* room details*/}
+        <div className="w-full mx-auto flex flex-col">
+          <h1 className="text-3xl font-bold">Join a room</h1>
+          <button
+            onClick={handleJoinRoom}
+            className="cursor-pointer border-2 border-[#403d71] px-5 py-2 max-w-[300px] mx-auto hover:bg-[#403d71] hover:text-[#8da4f1]"
+          >
+            Generate OrderId
+          </button>
+          {orderId && (
+            <div className="border-[1px solid #8da4f1] p-4 rounded-md shadow-md">
+              <h2 className="text-2xl font-bold">Room ID: {orderId}</h2>
+              <p>
+                Please wait for your driver to join this room. You can use the
+                provided room ID to find your driver on the app.
+              </p>
+            </div>
+          )}
+        </div>
+
+        {/* user location */}
+        <div className="w-full mx-auto mt-[2rem]">
+          <h2 className="text-3xl font-bold">Your Location</h2>
+          <div className="border-[1px solid #8da4f1] p-4 rounded-md shadow-md">
+            <p>Latitude: {userLocation.lat}</p>
+            <p>Longitude: {userLocation.lng}</p>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
